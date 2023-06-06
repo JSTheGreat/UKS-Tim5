cf2a01eb9fc8e822fbc22ae928d1448d37fae8d1
Nikola Petrovic <s.nino.petrovic@gmail.com>
2023-06-06 18:53:05 +0200

    init branch

--- UKS_Git_Site/GitApp/tests/test_user.py
+++ UKS_Git_Site/GitApp/tests/test_user.py
@@ -8,19 +8,18 @@ from ..models import User
 
 class LoginTestCase(TestCase):
     def setUp(self):
-        self.wrong_user = {
-            'username': 'testuser',
-            'password': 'secret'}
-        self.correct_user = {
-            'username': 'user1',
-            'password': 'user1'
-        }
-        User.objects.create(username="user1",password="user1",email="user1@user.com")
-        User.objects.create(username="user2",password="user2",email="user2@user.com")
-
-    # def test_login_positive(self):
-    #     response = self.client.post('/login/', self.correct_user, follow=True)
-    #     self.assertTrue(response.context['user'].is_authenticated)
+        self.username = 'user1'
+        self.password = 'user1'
+        self.user = User.objects.create(
+            username=self.username,
+            password=self.password
+        )
+    def test_login_positive(self):
+        login_url = reverse('login')  # Replace 'login' with your actual login URL name
+        response = self.client.post(login_url, {'username': self.username, 'password': self.password})
+        # self.assertEqual(response.status_code, 302)  # Check for a redirect status code (302)
+        # self.assertRedirects(response, reverse('index'))  # Replace 'index' with the actual URL name of the redirect target
+        # self.assertFalse(response.context['user'].is_authenticated)
 
     # def test_login_negative(self):
     #     response = self.client.post('/login/', self.wrong_user, follow=True)
